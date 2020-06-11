18b62fd3f9ea9de4f5b6838c773f3e8d204841c2
nik <nik_mak@bk.ru>
2020-06-11 21:51:36 +0300

    clear main activity

---
+++ Source/android/tinyJavaPosTester/src/com/shtrih/tinyjavapostester/network/ConnectToBluetoothDeviceTask.java
@@ -0,0 +1,101 @@
+package com.shtrih.tinyjavapostester.network;
+
+import android.app.ProgressDialog;
+import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
+
+import com.shtrih.fiscalprinter.ShtrihFiscalPrinter;
+import com.shtrih.jpos.fiscalprinter.FirmwareUpdateObserver;
+import com.shtrih.jpos.fiscalprinter.SmFptrConst;
+import com.shtrih.tinyjavapostester.JposConfig;
+import com.shtrih.tinyjavapostester.MainActivity;
+import com.shtrih.tinyjavapostester.MainViewModel;
+
+import java.util.HashMap;
+import jpos.JposConst;
+
+import static com.shtrih.tinyjavapostester.MainActivity.PROTOCOL;
+
+public class ConnectToBluetoothDeviceTask extends AsyncTask<Void, Void, String> {
+
+    private final MainActivity parent;
+    private final String address;
+    private final FirmwareUpdateObserver observer;
+    private final String timeout;
+    private final boolean fastConnect;
+    private final boolean scocFirmwareAutoupdate;
+    private final ShtrihFiscalPrinter printer;
+    private final MainViewModel model;
+
+    private long startedAt;
+    private long doneAt;
+
+    private ProgressDialog dialog;
+
+    public ConnectToBluetoothDeviceTask(MainActivity parent, String address, FirmwareUpdateObserver observer, String timeout, boolean fastConnect, boolean scocFirmwareAutoupdate, ShtrihFiscalPrinter printer, MainViewModel model) {
+        this.parent = parent;
+
+        this.address = address;
+        this.observer = observer;
+        this.timeout = timeout;
+        this.fastConnect = fastConnect;
+        this.scocFirmwareAutoupdate = scocFirmwareAutoupdate;
+        this.printer = printer;
+        this.model = model;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        parent.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+        dialog = ProgressDialog.show(parent, "Connecting to device", "Please wait...", true);
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        startedAt = System.currentTimeMillis();
+
+        try {
+            HashMap<String, String> props = new HashMap<>();
+            props.put("portName", address);
+            props.put("portType", "3");
+            props.put("portClass", "com.shtrih.fiscalprinter.port.BluetoothPort");
+            props.put("protocolType", PROTOCOL);
+            props.put("fastConnect", fastConnect ? "1" : "0");
+            props.put("capScocUpdateFirmware", scocFirmwareAutoupdate ? "1" : "0");
+            props.put("byteTimeout", timeout);
+
+            JposConfig.configure("ShtrihFptr", parent.getApplicationContext(), props);
+            if (printer.getState() != JposConst.JPOS_S_CLOSED) {
+                printer.close();
+            }
+            printer.open("ShtrihFptr");
+            printer.claim(3000);
+            printer.setDeviceEnabled(true);
+            model.ScocUpdaterStatus.set("");
+            printer.setParameter3(SmFptrConst.SMFPTR_DIO_PARAM_FIRMWARE_UPDATE_OBSERVER, observer);
+
+            return null;
+
+        } catch (Exception e) {
+            return e.getMessage();
+        } finally {
+            doneAt = System.currentTimeMillis();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        dialog.dismiss();
+
+        if (result == null)
+            parent.showMessage("Success " + (doneAt - startedAt) + " ms");
+        else
+            parent.showMessage(result);
+
+        parent.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+    }
+}
