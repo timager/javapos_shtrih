54fca22d58b43c04d9fa9fda6aa0dc191b0ef1a9
timager <timger98@gmail.com>
2020-06-25 13:21:22 +0300

    intent parsing

--- Source/android/tinyJavaPosTester/src/com/shtrih/tinyjavapostester/PayActivity.java
+++ Source/android/tinyJavaPosTester/src/com/shtrih/tinyjavapostester/PayActivity.java
@@ -2,11 +2,15 @@ package com.shtrih.tinyjavapostester;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.shtrih.tinyjavapostester.databinding.ActivityMainBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class PayActivity extends AppCompatActivity {
@@ -15,15 +19,21 @@ public class PayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_pay);
         Intent intent = getIntent();
-        TextView text1 = findViewById(R.id.textView);
-        TextView text2 = findViewById(R.id.textView2);
-        String str = intent.getStringExtra("url");
-        str = str == null ? "Нет ссылки" : str;
-        text1.setText(str);
-        if(intent.getData() != null){
-            text2.setText(intent.getData().toString());
-        }else{
-            text2.setText("Нет данных");
+        TextView dataTextField = findViewById(R.id.textView2);
+        if (intent.getData() != null) {
+            try {
+                JSONObject data = getJsonFromUrl(intent.getData());
+                dataTextField.setText(data.toString());
+            } catch (JSONException e) {
+                dataTextField.setText(e.getMessage());
+            }
+        } else {
+            dataTextField.setText("Нет данных");
         }
+    }
+
+    private JSONObject getJsonFromUrl(Uri url) throws JSONException {
+        String json = url.getQuery().replaceAll("[/]+$", "");
+        return new JSONObject(json);
     }
 }
