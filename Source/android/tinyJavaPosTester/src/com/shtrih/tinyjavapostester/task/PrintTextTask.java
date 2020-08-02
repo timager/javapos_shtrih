7e28eb4d5cee9661f4160ae6fd7188be93bd626c
nik <nik_mak@bk.ru>
2020-08-02 15:42:00 +0300

    refiscalization 2

--- Source/android/tinyJavaPosTester/src/com/shtrih/tinyjavapostester/task/PrintTextTask.java
+++ Source/android/tinyJavaPosTester/src/com/shtrih/tinyjavapostester/task/PrintTextTask.java
@@ -1,6 +1,7 @@
 package com.shtrih.tinyjavapostester.task;
 
 import com.shtrih.fiscalprinter.ShtrihFiscalPrinter;
+import com.shtrih.fiscalprinter.command.FSReFiscalization;
 import com.shtrih.jpos1c.xml.fnoperation.ParametersFiscal;
 import com.shtrih.tinyjavapostester.Fiscalizer;
 import com.shtrih.tinyjavapostester.MainViewModel;
@@ -25,35 +26,37 @@ public class PrintTextTask extends AbstractTask {
 
     protected void exec(ShtrihFiscalPrinter printer) throws JposException {
         printer.resetPrinter();
-        Fiscalizer fiscalizer = new Fiscalizer();
-        ParametersFiscal parameters = new ParametersFiscal();
-        parameters.CashierName = "Гераймович Николебн Максимович Погромизд";
-        parameters.CashierVATIN = "784105015925";
-        parameters.KKTNumber = "0000000001018733";
-        parameters.OrganizationName = "STb";
-        parameters.VATIN = "7842033384";
-        parameters.AddressSettle = "Улицы Сычова 25";
-        parameters.TaxVariant = "0,1,2,3";
-        parameters.OfflineMode = true;
-        parameters.DataEncryption = false;
-        parameters.SaleExcisableGoods = false;
-        parameters.SignOfGambling = false;
-        parameters.SignOfLottery = false;
-        parameters.BSOSing = false;
-        parameters.CalcOnlineSign = false;
-        parameters.PrinterAutomatic = false;
-        parameters.AutomaticMode = false;
-        parameters.ReasonCode = 2;
-        parameters.SignOfAgent = "0";
-        parameters.OFDOrganizationName = "ООО «ПЕТЕР-СЕРВИС Спецтехнологии»";
-        parameters.OFDVATIN = "7841465198";
-
-
-        try {
-            fiscalizer.refiscalizeFS(printer, parameters);
-        } catch (Exception e) {
-            parent.showMessage(e.getMessage());
-        }
+        FSReFiscalization command = new FSReFiscalization(printer.getSysPassword(), "784105015925", "", 2, 1, 2);
+        printer.executeCommand(command);
+//        Fiscalizer fiscalizer = new Fiscalizer();
+//        ParametersFiscal parameters = new ParametersFiscal();
+//        parameters.CashierName = "Гераймович Николебн Максимович Погромизд";
+//        parameters.CashierVATIN = "784105015925";
+//        parameters.KKTNumber = "0000000001018733";
+//        parameters.OrganizationName = "STb";
+//        parameters.VATIN = "7842033384";
+//        parameters.AddressSettle = "Улицы Сычова 25";
+//        parameters.TaxVariant = "0,1,2,3";
+//        parameters.OfflineMode = true;
+//        parameters.DataEncryption = false;
+//        parameters.SaleExcisableGoods = false;
+//        parameters.SignOfGambling = false;
+//        parameters.SignOfLottery = false;
+//        parameters.BSOSing = false;
+//        parameters.CalcOnlineSign = false;
+//        parameters.PrinterAutomatic = false;
+//        parameters.AutomaticMode = false;
+//        parameters.ReasonCode = 2;
+//        parameters.SignOfAgent = "0";
+//        parameters.OFDOrganizationName = "ООО «ПЕТЕР-СЕРВИС Спецтехнологии»";
+//        parameters.OFDVATIN = "7841465198";
+//
+//
+//        try {
+//            fiscalizer.refiscalizeFS(printer, parameters);
+//        } catch (Exception e) {
+//            parent.showMessage(e.getMessage());
+//        }
         printer.printText(text);
         parent.showMessage(String.valueOf(printer.fsReadStatus().getStatus().getCode()));
     }
