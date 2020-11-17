05990acfaecc8b005d54ee3a863993244733fee7
Alex Z <Aleks-Z@users.noreply.github.com>
2020-11-17 03:08:34 +0300

    * fix

--- Source/android/tinyJavaPosTester/src/com/shtrih/tinyjavapostester/util/ToastUtil.java
+++ Source/android/tinyJavaPosTester/src/com/shtrih/tinyjavapostester/util/ToastUtil.java
@@ -1,18 +1,23 @@
 package com.shtrih.tinyjavapostester.util;
 
 import android.app.Application;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.StringRes;
 import android.widget.Toast;
 
 public class ToastUtil {
     private static Application app;
+    private static Handler mMainHandler = new Handler(Looper.getMainLooper());
 
     public static void init(Application application) {
         app = application;
     }
 
     public static void showMessage(String message) {
-        Toast.makeText(app, message, Toast.LENGTH_SHORT).show();
+        mMainHandler.post(() -> {
+            Toast.makeText(app, message, Toast.LENGTH_SHORT).show();
+        });
     }
 
     public static void showMessage(@StringRes Integer resId) {
